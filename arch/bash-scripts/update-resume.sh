bda998441d965146e5f6f870694b20f5ddff4ab3
Joao Lucas Flauzino Cassiano <joaolucas.apoio@gmail.com>
2025-09-23 13:16:00 -0300

    feature
    gemini resume after arch system updates

---
+++ arch/bash-scripts/update-resume.sh
@@ -0,0 +1,5 @@
+function update-summary() {
+	sudo pacman -Syu | tee /dev/tty | gemini -m gemini-2.5-flash -p "Resuma o seguinte output de uma atualização do Arch Linux. Foque nos pacotes que foram atualizados,
+  novos pacotes instalados e pacotes removidos. Ignore as barras de progresso e detalhes de download. Apresente um sumário conciso em português e se possível,
+  me fale se tem alguma chance de quebrar meu sistema com alguma atualização realizada:"
+}
